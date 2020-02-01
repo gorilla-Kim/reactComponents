5767ba05a67a3ce70b54038f7e802cc70c968f39
gorilla-Kim <00ghks22@naver.com>
2020-02-02 00:38:53 +0900

    seperate UserList and UserItem component

--- admin-site/src/App.js
+++ admin-site/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 // import './App.css';
-import UsersContainer from './containers/UsersContainer';
-import UserList from './components/UserList';
 import UserListContainer from './containers/UserListContainer';
 
 function App() {
--- admin-site/src/components/UserList.js
+++ admin-site/src/components/UserList.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+export const UserItem = ({ user }) => {
+  return (
+    <li key={user.key}>
+      {user.name}
+    </li>
+  )
+}
+
 export default function UserList({ users }) {
   return (
     <ul>
       {
-        users.map(user => <li key={user.id}>{user.name}</li>)
+        users.map(user => <UserItem user={user}/> )
       }
     </ul>
   )

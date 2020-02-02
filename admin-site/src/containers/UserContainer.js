4f218d1e05a05839c258a4973486d591ad1d19b2
gorilla-Kim <00ghks22@naver.com>
2020-02-02 21:49:45 +0900

    update get_user function 필요시 마다 요청했던 각 개별 유저에 대한 데이터를 다시 재요청하지 않도록 설정

--- admin-site/src/containers/UserContainer.js
+++ admin-site/src/containers/UserContainer.js
@@ -2,16 +2,20 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import User from '../components/User';
 import { getUser } from '../modules/users';
+import { reducerUtils } from '../lib/asyncUtils';
 
 export default function UserContainer({ userId }) {
-  const { data, loading, error } = useSelector( state => state.users.user );
+  const { data, loading, error } = useSelector( 
+    state => state.users.user[userId] || reducerUtils.initial()
+  );
   const dispatch = useDispatch();
 
   useEffect(()=>{
+    if(data) return;
     dispatch(getUser(userId));
-  }, [userId, dispatch]);
+  }, [userId, dispatch, data]);
 
-  if(loading) return <div>loading....</div>
+  if(loading && !data) return <div>loading....</div>
   if(error) return <div>error occure!!</div>
   if(!data) return null;
 
--- admin-site/src/modules/users.js
+++ admin-site/src/modules/users.js
@@ -8,14 +8,34 @@ const GET_USERS_ERROR = 'user/GET_USERS_ERROR';
 const GET_USER = 'user/GET_USER';
 const GET_USER_SUCCESS = 'user/GET_USER_SUCCESS';
 const GET_USER_ERROR = 'user/GET_USER_ERROR';
+const CLEAR_USER = 'user/CLEAR_USER';
 
 const ADD_USER = 'user/ADD_USER';
 const UPDATE_USER = 'user/UPDATE_USER';
 const DELETE_USER = 'user/DELETE_USER';
 
-export const getUsers = createPromiseThunk(GET_USERS, usersAPI.getUsers) 
+export const getUsers = createPromiseThunk(GET_USERS, usersAPI.getUsers);
+// export const getUser = createPromiseThunk(GET_USER, usersAPI.getUserById);
+export const getUser = id => async dispatch => {
+    dispatch({ type: GET_USER, meta: id });
+    try {
+        const payload = await usersAPI.getUserById(id);;
+        dispatch({
+            type: GET_USER_SUCCESS,
+            payload,
+            meta: id
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_USER_ERROR,
+            payload: error,
+            error: true,
+            meta: id
+        });
+    }
+}
 
-export const getUser = createPromiseThunk(GET_USER, usersAPI.getUserById)
+export const clearUser = () => ({ type: CLEAR_USER });
 
 // action
 let nextID = 1;
@@ -35,12 +55,44 @@ export const deleteUser = id => ({ type: DELETE_USER, id });
 // initialState
 const initialState = {
     users: reducerUtils.initial(),
-    user: reducerUtils.initial()
+    // user: reducerUtils.initial()
+    user: {}
 };
 
 // reducer
-const getUsersReducer = handleAsyncActions(GET_USERS, 'users');
-const getUserReducer = handleAsyncActions(GET_USER, 'user');
+const getUsersReducer = handleAsyncActions(GET_USERS, 'users', true);
+// const getUserReducer = handleAsyncActions(GET_USER, 'user');
+const getUserReducer = (state, action) => {
+    const id = action.meta;
+    switch (action.type) {
+        case GET_USER:
+            return {
+                ...state,
+                user: {
+                    ...state.user,
+                    [id] : reducerUtils.loading( state.user[id] && state.user[id].data )
+                }
+            }
+        case GET_USER_SUCCESS:
+            return {
+                ...state,
+                user: {
+                    ...state.user,
+                    [id] : reducerUtils.success( action.payload )
+                }
+            }
+        case GET_USER_ERROR:
+            return {
+                ...state,
+                user: {
+                    ...state.user,
+                    [id] : reducerUtils.error( action.payload )
+                }
+            }
+        default:
+            return state;
+    }
+}
 
 export default function users(state = initialState, action) {
     switch (action.type) {
@@ -70,7 +122,11 @@ export default function users(state = initialState, action) {
         case GET_USER_SUCCESS:
         case GET_USER_ERROR:
             return getUserReducer(state, action)
-
+        case CLEAR_USER:
+            return {
+                ...state,
+                user: reducerUtils.initial()
+            }
         default:
             return state;
     }

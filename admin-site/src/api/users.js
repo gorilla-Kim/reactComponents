a6e2649a3364fed9f4d2d713206622153613752d
gorilla-Kim <00ghks22@naver.com>
2020-02-03 01:41:48 +0900

    proxy 설정

--- admin-site/src/api/users.js
+++ admin-site/src/api/users.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 // const sleep = n => new Promise(resolve => setTimeout(resolve, n));
 
 export const getUsers = async () => {
-  const response = await axios.get('http://localhost:4000/users');
+  const response = await axios.get('/users');
   return response.data;
 }
 
 export const getUserById = async id => {
-  const response = await axios.get(`http://localhost:4000/users/${id}`);
+  const response = await axios.get(`/users/${id}`);
   return response.data;
 }
